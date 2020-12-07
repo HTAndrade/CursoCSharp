7dfd11fa84236c76558488bc8f078c4fab331058
Hugo Andrade <71911876+HTAndrade@users.noreply.github.com>
2020-12-07 11:43:09 -0300

    Initial commit

---
+++ CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    class PrimeiroPrograma {
+        static void Main() {
+            Console.Write("Primeiro ");        //Write: Escreve o texto e mantem na mesma linha
+            Console.WriteLine("Programa");     //WriteLine: Escreve o texto e quebra a linha
+            Console.WriteLine("Terminou!");
+        }
+    }
+}
