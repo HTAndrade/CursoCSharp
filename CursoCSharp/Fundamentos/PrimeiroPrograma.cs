d89148594b7544474df4fa3855433c128cd58841
Hugo Andrade <71911876+HTAndrade@users.noreply.github.com>
2021-02-01 02:13:20 -0300

    Atualização de arquivos

--- CursoCSharp/Fundamentos/PrimeiroPrograma.cs
+++ CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace CursoCSharp.Fundamentos {
     class PrimeiroPrograma {
-        static void Main() {
+        static void Executar() {
             Console.Write("Primeiro ");        //Write: Escreve o texto e mantem na mesma linha
             Console.WriteLine("Programa");     //WriteLine: Escreve o texto e quebra a linha
             Console.WriteLine("Terminou!");
